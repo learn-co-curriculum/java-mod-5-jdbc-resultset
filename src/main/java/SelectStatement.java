ed075c11da872b97d9fc2f39f282067db936c939
Linda Seiter <linseiter@gmail.com>
2022-11-18 09:55:26 -0500

    new lesson jdbc select statement and ResultSet

---
+++ src/main/java/SelectStatement.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+public class SelectStatement {
+
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/employee_db";
+    static final String USER = "postgres";
+    static final String PASSWORD = "postgres";
+
+    public static void main(String[] args) {
+        try {
+            //Establish a connection to the database
+            Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+            //Create a statement object to execute SQL statements through the database connection
+            Statement statement = connection.createStatement();
+
+            //The method executeQuery returns a ResultSet object, which encapsulates the set of rows returned from the query.
+            ResultSet rs = statement.executeQuery("SELECT * FROM employee");
+
+            //Loop to get each row in the result set.  The method rs.next() moves a cursor to next row.
+            while (rs.next()) {
+                //Use the column name to retrieve the value from the current row
+                System.out.println(
+                            String.format("id %d email %s office %s salary %.2f",
+                                    rs.getInt("id"),
+                                    rs.getString("email"),
+                                    rs.getString("office"),
+                                    rs.getDouble("salary")));
+            }
+            rs.close();  //close the result set
+
+            connection.close();  //close the database connection
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
